e4caa6d09b2e910d4ea8a805fb5c95be5a7b821e
Egor Kirin <egorcom27@mail.ru>
2022-01-26 22:24:30 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,115 @@
+const audio = new Audio();
+
+document.querySelector('#audio').addEventListener('click', function(e) {
+	if (e.target.value === 'LO-FI') {
+		e.target.value = 'STOP';
+		audio.src = e.target.getAttribute('data-url');
+		audio.play();
+	} else {
+		e.target.value = 'LO-FI';
+		audio.currentTime = 0;
+		audio.pause();
+	}
+});
+
+document.querySelector('#audio2').addEventListener('click', function(e) {
+	if (e.target.value === 'ROCK') {
+		e.target.value = 'STOP';
+		audio.src = e.target.getAttribute('data-url');
+		audio.play();
+	} else {
+		e.target.value = 'ROCK';
+		audio.currentTime = 0;
+		audio.pause();
+	}
+});
+
+/*--------------------------------*/ 
+
+const alarmSound = new Audio('../music/Alarm.mp3');
+// DOM buttons and clock
+const startBtn = document.querySelector('#start-btn');
+const stopBtn = document.querySelector('#stop-btn');
+const workAndBreakButtons = Array.from(document.getElementsByClassName('timer-option'));
+const clockFace = document.querySelector('#time');
+const secondsElem = document.querySelector('#seconds');
+const minutesElem = document.querySelector('#minutes');
+
+let minutes = document.querySelector('#minutes');
+let seconds = document.querySelector('#seconds');
+let minReal = 0;
+let secReal = 0;
+let progressStart = 0;
+let progressEnd = calcProgressEnd(minutes.innerHTML, seconds.innerHTML);
+let isRunning = false;
+let progressName;
+
+function displayTime() {
+	minReal = Math.floor((progressEnd - progressStart) / 60);
+	secReal = Math.floor((progressEnd - progressStart) % 60);
+
+	minutesElem.innerHTML = minReal.toString().length == 2 ? minReal : `0${minReal}`;
+	secondsElem.innerHTML = secReal.toString().length == 2 ? secReal : `0${secReal}`;
+
+	document.title = `${minReal}:${secReal}`;
+}
+
+
+function startTimer() {
+	progressName = setInterval(() => {
+		progressStart++;
+		displayTime();
+		if (progressStart >= progressEnd) {
+			alarmSound.play();
+			clearInterval(progressName);
+			isRunning = false;
+			displayTime();
+			progressStart = 0;
+		}
+	}, 1000);
+}
+
+function playOrStop(e) {
+	if (isRunning && e.target.innerHTML === 'Stop') {
+		clearInterval(progressName);
+		isRunning = false;
+	} else if (!isRunning && e.target.innerHTML !== 'Stop') {
+		minReal === startTimer();
+		isRunning = true;
+	}
+}
+
+startBtn.addEventListener('click', playOrStop);
+stopBtn.addEventListener('click', playOrStop);
+workAndBreakButtons.forEach((button) => {
+	button.addEventListener('click', function () {
+		clearInterval(progressName);
+		switch (button.id) {
+			case 'work-session':
+				minutes.innerHTML = '25';
+				seconds.innerHTML = '00';
+				break;
+			case 'short-break':
+				minutes.innerHTML = '05';
+				seconds.innerHTML = '00';
+				break;
+			case 'long-break':
+				minutes.innerHTML = '15';
+				seconds.innerHTML = '00';
+				break;
+		}
+		isRunning = false;
+		progressEnd = calcProgressEnd(minutes.innerHTML, seconds.innerHTML);
+		progressStart = 0;
+		displayTime();
+	});
+});
+
+workAndBreakButtons.forEach((button) => {
+	button.addEventListener('click', playOrStop);
+});
+
+function calcProgressEnd(min, sec) {
+	return parseInt(min) * 60 + parseInt(sec);
+}
+
